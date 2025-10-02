f15efdf224f2e97b627ecd156b0a0a478047ae35
justmore5mins <justmore5mins@gmail.com>
2025-10-02 21:16:53 +0800

    add

--- ExampleProject/src/main/java/frc/robot/Shooter/Shooter.java
+++ ExampleProject/src/main/java/frc/robot/Shooter/Shooter.java
@@ -2,10 +2,13 @@ package frc.robot.Shooter;
 
 import static edu.wpi.first.units.Units.Seconds;
 
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class Shooter {
+    public SparkMax ShooterMotor;
     public Command shoot(){
         return Commands.none().withTimeout(Seconds.of(1));
     }
